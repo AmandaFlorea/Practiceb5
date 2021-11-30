37fc4bd6106ecc23e00b8991b20924219484dbb5
agg72 <agg72@DESKTOP-BN6NDO2>
2021-11-30 17:09:27 -0500

    added value to addNum()

--- src/one/PracticeOne.java
+++ src/one/PracticeOne.java
@@ -14,6 +14,8 @@ public class PracticeOne {
 	}
 	
 	public static void addNum(int a, int b) {
+		a = 15;
+		b = 20;
 		System.out.println(a + b);
 	}
 	public static void getPractice(String name) {
